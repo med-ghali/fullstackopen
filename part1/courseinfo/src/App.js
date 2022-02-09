6908b3c0fb6dae26f171101c4a85d1d973a4834e
med-ghali <tagemouati.medghali@gmail.com>
2022-02-09 17:22:22 +0100

    correction

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,27 +1,47 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-const Header = ({text}) => (<h1>text</h1>) ;
 
-const Button = ({text,handleClick}) => {
-  return (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
-};
+const Header = (props) =>  (<h1>{props.course}</h1>) ;
+const Part = (props) => (<p> {props.part.name} {props.part.exercises} </p>) ;
+const Content  = (props) => ( 
+  <div>
+    <Part part={props.parts[0]}  />
+    <Part part={props.parts[1]}  />
+    <Part part={props.parts[2]}  />
+  </div>
+);
+const Total = (props) => {
+let a = 0 ;
+props.parts.forEach( b => { a+= b.exercises ;  });
+return (<p>Number of exercises {a} </p>) ; };
+
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header text="give feedback"> </Header>
-   
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
 
-export default Ap
+export default App
