292cf4eb236ca8df1d7c3d4432f0f6ad84f50065
med-ghali <tagemouati.medghali@gmail.com>
2023-08-19 19:36:40 +0100

    unicafe

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const Header = ({course}) =>{
+	return (
+		<>
+			<h1>{course}</h1>
+		</>
+	)
 }
 
-export default App
+const Part = ({part,exercise}) => {
+	return (
+		<>
+			 <p>{part} {exercise} </p>
+		</>
+	)
+}
+
+const Content  = ({parts}) =>{
+	const dataList = parts.map( data => 
+		<Part key={data.id} part={data.name} exercise={data.exercises}/>	
+	)
+	return (
+		<>
+			{dataList}
+		</>
+	)
+}
+
+const Total = ({parts}) =>{
+	const sum = parts.reduce( (a,b) => (a + b.exercises), 0);
+	return (
+		<>
+			<p>Number of exercises {sum}</p>
+		</>
+	)
+}
+
+const App = () => {
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+		  {
+			id: 1,
+			name: 'Fundamentals of React',
+			exercises: 10
+		  },
+		  {
+			id: 2,
+			name: 'Using props to pass data',
+			exercises: 7
+		  },
+		  {
+			id: 3,
+			name: 'State of a component',
+			exercises: 14
+		  }
+		]
+	  }
+	  return (
+		<div>
+		  <Header course={course.name} />
+		  <Content parts={course.parts} />
+		  <Total parts={course.parts} />
+		</div>
+	  )
+  }
+  
+  export default App
--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const StatisticLine = ({text,value}) => {
+	return (
+		<tbody>
+			<tr>
+				<td>{text}</td>
+				<td>{value}</td>
+			</tr>
+		</tbody>
+	)
+}
+
+const Statistics = ({good,neutral,bad}) => {
+	const all = good + bad + neutral;
+	if (all === 0)
+		return (<p>no feedback given</p>)
+	const positive = (good/all) * 100;
+	const average = (good - bad) / all;
+	return (
+		<>
+			<h1> Statistics</h1>
+			<table>
+				<StatisticLine text="good" value={good}/>
+				<StatisticLine text="neutral" value={neutral}/>
+				<StatisticLine text="bad" value={bad}/>
+				<StatisticLine text="all" value={all}/>
+				<StatisticLine text="average" value={average}/>
+				<StatisticLine text="positive" value={positive + " %"}/>
+			</table>
+		</>
+	)
+}
+
+const Button = ({handleClick,text}) => {
+	return (
+		<button onClick={handleClick}> {text} </button>
+	)
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+		<h1>give feedback</h1>
+		<div>
+			<Button text="good" handleClick={() => setGood(good + 1) } />
+			<Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+			<Button text="bad" handleClick={() => setBad(bad + 1) } />
+		</div>
+		<Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
   )
 }
 
