39253da215cb6c762e02b8f6f84c3864e4ca7bb8
med-ghali <tagemouati.medghali@gmail.com>
2023-08-25 18:41:47 +0100

    test

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 
+console.log("i am the newest version")
+
 const app = express()
 app.use(cors())
 app.use(express.static('./Staticdist'))
+
 
 app.use(express.json())
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
