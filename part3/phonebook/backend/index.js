470a2a298dea3cb1b84da2942510fb4d370d9ba6
med-ghali <tagemouati.medghali@gmail.com>
2023-08-26 22:20:21 +0100

    connecting delete  methods with db

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -16,37 +16,46 @@ app.use(express.static('dist'))
 const noteFound = "<h1>404 :Not Found</h1>"
 
 
-app.get("/api/persons", (request,response) => {
+app.get("/api/persons", (request,response,next) => {
 	Person.find({}).then((persons) => {
 		response.json(persons);
-	})
-})
-
-app.get("/api/persons/:id",  (request,response) => {
-	const id = request.params.id
-	Person.findById(id).then( person => {
-			response.json(person)
 	}).catch ((error) => {
-		response.status(404).send(noteFound)
+		next(error)
 	})
 })
 
-app.get("/api/info", (request,response) => {
+app.get("/api/persons/:id",  (request,response,next) => {
+	Person.findById(request.params.id).then( person => {
+		if (person)
+			response.json(person)
+		else
+		response.status(404).send(noteFound)
+	}).catch ((error) => {
+		next(error)
+	})
+})
+
+app.get("/api/info", (request,response,next) => {
 	Person.find({}).then((persons) => {
 		const p = new Date();
 		const peopleInfo = `<p> Phonebook has info for ${persons.length} people </p>`
 		const dateString = `<p> ${p.toString()} </p>`
 		response.send(peopleInfo + dateString)
+	}).catch ((error) => {
+		next(error)
 	})
 })
 
-app.delete("/api/persons/:id",  (request,response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter(person => person.id !== id)
-	response.status(204).end()
+app.delete("/api/persons/:id",  (request,response,next) => {
+	const id = request.params.id
+	Person.findByIdAndDelete(id).then ( (result) =>{
+		response.status(204).end()
+	}).catch (error => {
+		next(error)
+	})
 })
 
-app.post("/api/persons", (request,response) => {
+app.post("/api/persons", (request,response,error) => {
 	let body = request.body
 	if (!body.name || !body.number)
 	{
@@ -59,14 +68,21 @@ app.post("/api/persons", (request,response) => {
 	})
 	person.save().then( 
 		savedPerson => response.json(savedPerson)
-	)
+	).catch ((error) => {
+		next(error)
+	})
 })
 
-
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message)
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	}  
+	next(error)
+}
+  
+app.use(errorHandler)
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
-// "buildui": "shx rm -rf dist && cd ../frontend && npm run build && shx cp -r dist ../backend",
-// "deployfull": "npm run buildui && git add . && git commit -m uibuild && git push"
