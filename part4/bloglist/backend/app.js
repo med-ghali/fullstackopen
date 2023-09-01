bfbb9e1c5a606973836df7dc93ffe845af2fcbab
med-ghali <tagemouati.medghali@gmail.com>
2023-09-01 15:24:24 +0100

    more tests for the backend

--- part4/bloglist/backend/app.js
+++ part4/bloglist/backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const mongoUrl = require('./utils/config').DB_URL
 const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 mongoose.connect(mongoUrl).then( () => {
 	console.log("connected to db");
@@ -14,6 +16,7 @@ mongoose.connect(mongoUrl).then( () => {
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs',blogsRouter)
+app.use(middleware.errorHandler)
 
 
 module.exports = app
--- part4/bloglist/backend/controllers/blogs.js
+++ part4/bloglist/backend/controllers/blogs.js
@@ -1,6 +1,6 @@
 const blogsRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require('../models/blog')
-require('express-async-errors')
 
 blogsRouter.get('/', async (request, response) => {
 	const blogs = await Blog.find({})
@@ -9,13 +9,29 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.get('/:id', async (request, response) => {
 	const blog = await Blog.findById(request.params.id)
-	response.json(blog)
+	if (blog) {
+		response.json(blog)
+	} else {
+		response.status(404).end()
+	}
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+	await Blog.findByIdAndDelete(request.params.id)
+	response.status(204).end()
 })
 
 blogsRouter.post('/', async (request, response) => {
 	const blog = new Blog(request.body)
 	const result = await blog.save()
 	response.status(201).json(result)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+	const newBlog = request.body
+	console.log(newBlog)
+	updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, {new:true})
+	response.json(updatedBlog)
 })
 
 module.exports = blogsRouter
