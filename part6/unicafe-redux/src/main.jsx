40e08149ef6c4dbf5a582c0f62cd29ee72987b43
Mohamed El Ghali Tagemouati <tagemouati.medghali@gmail.com>
2023-09-16 09:53:54 +0100

    LINT

--- part6/unicafe-redux/src/main.jsx
+++ part6/unicafe-redux/src/main.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { createStore } from "redux"
+import reducer from "./reducer"
 
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const handleClick = (type) => {
     store.dispatch({
-      type: 'GOOD'
+      type,
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => handleClick("GOOD")}>good</button>
+      <button onClick={() => handleClick("OK")}>ok</button>
+      <button onClick={() => handleClick("BAD")}>bad</button>
+      <button onClick={() => handleClick("ZERO")}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
 const renderApp = () => {
   root.render(<App />)
